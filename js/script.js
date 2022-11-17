15c6645e2edb634b9a90b43e0dce5f105e7cf122
Yari <narducciyarigmail.com>
2022-11-17 14:50:30 +0100

    imissioni function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 //Campo Minato
+
+//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta
+
+//nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+
+//se clicchiamo la bomba la cella si colora di rosso e termina la partita con apparizione scritta e punteggio
+
+//Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
+//Se clicchiamo tutte le caselle senza bombe apparirà una scritta che indicherà il punteggio
+
+
     //Creiamo la variabile container dove appenderemo i nostri elementi
 let container = document.getElementById("container-grid");
 
-console.log(container);
+// console.log(container);
 //Creiamo la variabile della difficoltà
 let difficult = document.getElementById("difficoltà");
 //Agganciamo la variabile bottone 
@@ -13,12 +25,16 @@ let myBtn = document.querySelector("#play");
 myBtn.addEventListener("click",
 function (){
 
+   
+
     //Creiamo le condizioni per le varie difficolta sottolineando il value
 
     if(difficult.value == "normal"){
+       
         container.innerHTML= "";
         for(let i=1 ; i<=100; i++){
-            
+            let bombElement = createArrayNum(16, 1, i);
+
             let nuovoElemento = createBox("div","box");
             
             container.appendChild(nuovoElemento);
@@ -33,10 +49,6 @@ function (){
              }
              )
            
-            
-            
-        
-            
         }
     } 
     if(difficult.value == "hard"){
@@ -55,7 +67,7 @@ function (){
                 nuovoElemento.classList.add("clicked");
                 nuovoElemento.innerHTML = i;
              
-                console.log("Stai cliccando il numero",i);
+                 console.log("Stai cliccando il numero",i);
              }
              )
            
@@ -109,3 +121,18 @@ function createBox(){
 
     return divElement;
 }
+function randomNumber(min, max) {
+    let randomNum = Math.floor(Math.random() * ((max + 1) - min) + min);
+    return randomNum
+}
+
+function createArrayNum(numPossibilità, minNum, maxNum) {
+    let arrayNum = [];
+    while (arrayNum.length < numPossibilità) {
+        let randomNumForArray = randomNumber(minNum, maxNum);
+        if (!arrayNum.includes(randomNumForArray))
+            arrayNum.push(randomNumForArray);
+
+    }
+    return arrayNum;
+};
